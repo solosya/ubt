538292d71630c86aeb93ab8630ea83112344b054
Lee Neenan <lneenan@pagemasters.com.au>
2017-08-02 14:38:46 +1000

    mobile almost done

--- static/development/js/scripts.js
+++ static/development/js/scripts.js
@@ -17,6 +17,47 @@ $('document').ready(function() {
 
 
 
+    function formatTo12hrTime(date) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      return hours + ':' + minutes + ampm;
+    }
+
+
+    function formatDate(date) {
+        var monthNames = [
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+        ];
+
+        var dayNames = [
+            "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"
+        ];
+
+        var day = date.getDate() + ',';
+        var daystring = dayNames[date.getDay()];
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+        var time = formatTo12hrTime(date);
+        var output = [time, daystring, monthNames[monthIndex], day, year];
+        return output.join(' ').toUpperCase();
+    }
+
+    // 4:32PM WEDNESDAY JULY 4, 2017
+    var date = new Date();
+        datetime = date.toISOString().substring(0, 16),
+        field = document.getElementById('headerTime');
+        field.setAttribute('datetime', datetime);
+        field.innerHTML = formatDate(date);
+
+
     // var isMobile = function(){
     //     if (window.innerWidth < mobileView) {
     //         return true;
