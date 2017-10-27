d8c7e1cdccbc06fb02e088ece35b07cb9ddce30c
Lee Neenan <lneenan@pagemasters.com.au>
2017-10-27 13:39:42 +1100

    match job/property article layouts in js

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -32514,7 +32514,6 @@ Acme.filteredListingViewClass.prototype.listeners = {
     }
 };
 Acme.filteredListingViewClass.prototype.render = function() {
-    console.log('rendering after search');
     var container = this.container;
     var cardClass = "card-rec-jobs card-rec-jobs-tablet card-rec-jobs-mobile";
 
@@ -32610,14 +32609,13 @@ ListingForm.constructor = ListingForm;
         this.menus.buyMenu = new Acme.listMenu({
                     'parent'        : $('#buySelect'),
                     'list'          : contractList,
-                    'defaultSelect' : {"label": 'Bye/lease'},
+                    'defaultSelect' : {"label": 'Buy/lease'},
                     'name'          : 'contracttype',
                     'key'           : 'extendedData.contracttype'
         }).init().render();
     };
     ListingForm.prototype.render = function() 
     {
-        console.log('rendering');
         var form = this.container.main;
         var title = form.find("#title");
         var content = form.find("#content");
@@ -32702,7 +32700,6 @@ ListingForm.constructor = ListingForm;
     },
     ListingForm.prototype.events = function() 
     {
-        console.log('calling events!!');
         var self = this;
         $('input, textarea').on("change", function(e) {
             e.stopPropagation();
@@ -32766,15 +32763,12 @@ ListingForm.constructor = ListingForm;
             e.preventDefault();
 
             var validated = self.validate();
-            console.log(validated);
             if (!validated) {
                 self.render();
-                console.log('rendering and returning');
                 return;
             }
 
             self.data.theme_layout_name = self.layout;
-            console.log('article.create!!!', self.data);
             Acme.server.create('/api/article/create', self.data).done(function(r) {
                 $('#listingFormClear').click();
                 Acme.PubSub.publish('update_state', {'userArticles': ''});
@@ -33031,6 +33025,167 @@ Acme.PropertyForm = function(blogId, layout) {
 };
 Acme.PropertyForm.prototype = new ListingForm();
 Acme.PropertyForm.prototype.constructor=Acme.PropertyForm;
+
+
+
+
+
+
+
+
+
+
+Acme.EventForm = function(blogId) {
+        this.subscriptions = Acme.PubSub.subscribe({
+            'Acme.eventForm.listener' : ['state_changed', 'update_state']
+        });
+
+        this.errorFields = [];
+
+        this.compulsoryFields = [
+            "title", 
+            "content" 
+        ];
+
+        this.blogId = blogId;
+
+        this.data = {
+            'id': 0,
+            'blogs': this.blogId,
+            'media_ids': '',
+            'type': 'event'
+        };
+
+        this.events();
+        this.events2();
+    }
+    Acme.EventForm.prototype = new ListingForm();
+    Acme.EventForm.prototype.constructor=Acme.EventForm;
+    Acme.EventForm.listeners = 
+    {
+        "start_date" : function(data, topic) {
+            this.data.start_date = data['start_date'];
+        },
+        "end_date" : function(data, topic) {
+            this.data.end_date = data['end_date'];
+        },
+        "after" : function(data, topic) {
+            console.log(this.data);
+        }
+    };
+    Acme.EventForm.prototype.events2 = function() {
+
+        $('#start_date, #end_date').datetimepicker({
+            format: "DD-MM-YYYY h:mm A",
+            useCurrent: false,
+            icons: {
+                time: "fa fa-clock-o",
+                date: "fa fa-calendar",
+                up: "fa fa-angle-up",
+                down: "fa fa-angle-down"
+            },
+            tooltips: {selectTime: ''}
+        }).on('dp.change', function (e) {
+            var data = {};
+            data[e.target.id] = e.date.format('YYYY-MM-DD HH:mm');
+            if(data['start_date'] || data['end_date']) {
+                $('#end_date').data("DateTimePicker").minDate(e.date);
+                Acme.PubSub.publish("update_state", data);
+            }
+        });
+
+        var EventPostGoogleMap = function () {
+            var marker, geocoder;
+            var elem = $('#addressMap');
+            var latitude = elem.data('latitude');
+            var longitude = elem.data('longitude');
+            var map;
+            
+            //google.maps.event.addDomListener(window, 'load', initMap);
+            function initMap() {
+                var mapLat;
+                var mapLong;
+                if (latitude !== '' && longitude !== '') {
+                    mapLat = latitude;
+                    mapLong = longitude;
+
+                    geocoder = new google.maps.Geocoder();
+                    map = new google.maps.Map(document.getElementById('addressMap'), {
+                        zoom: 10,
+                        center: {lat: mapLat, lng: mapLong}
+                    });
+
+                    //set current marker
+                    if (latitude != '' && longitude != '') {
+                        updateMarker = new google.maps.Marker({
+                            position: new google.maps.LatLng(latitude, longitude),
+                            map: map
+                        });
+                    }
+                } 
+                else {
+                    //navigator.geolocation.getCurrentPosition(function (position) {});
+                    geocoder = new google.maps.Geocoder();
+                    map = new google.maps.Map(document.getElementById('addressMap'), {
+                        zoom: 1,
+                        center: {lat: 43.197167, lng: 56.425781}
+                    });
+                    
+                }
+                
+                pointLocation(geocoder, map, marker);
+            }
+            
+            initMap();
+        };
+
+        var pointLocation = function (geocoder, map, marker) {
+            $('#address').on('change', function(e){
+                mapLocation();
+            });
+            
+            function mapLocation() {
+                var address = $('#address').val();
+
+                geocoder.geocode({address: address}, function (results, status) {
+                    
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        map.setCenter(results[0].geometry.location);
+                        map.setZoom(10);
+
+                        //clear the previous marker
+                        if (marker) {
+                            marker.setMap(null);
+                        }
+                        marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location
+                        });
+                        
+                        // Set Lat and Long
+                        var latitude = results[0].geometry.location.lat();
+                        var longitude = results[0].geometry.location.lng();
+                        $('#event_latitude').val(latitude);
+                        $('#event_longitude').val(longitude);
+                    } 
+                });
+            } 
+        };
+
+        EventPostGoogleMap();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
