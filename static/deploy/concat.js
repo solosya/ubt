43f6d1182f6e41a3f455fb5072608d1d904b9e92
Lee Neenan <lneenan@pagemasters.com.au>
2017-11-24 13:26:39 +1100

    update

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -33563,10 +33563,10 @@ Card.prototype.screen = function()
 
     run();
 
-    setInterval( run, 10000 ); 
-    setInterval( function() {
-        location.reload(false);
-    } , pageRefreshInterval );
+    // setInterval( run, 10000 ); 
+    // setInterval( function() {
+    //     location.reload(false);
+    // } , pageRefreshInterval );
  
 };
 
