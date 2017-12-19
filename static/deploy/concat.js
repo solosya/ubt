420ba8abe4a41c835c861fd4b8a5f8a1c26ec336
Alex Solosy <ASolosy@pagemasters.com.au>
2017-12-19 15:42:24 +1100

    no message

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -36707,7 +36707,8 @@ UserProfielController.Load = (function ($) {
                 console.log(secondDate.getFullYear() +'-'+secondDate.getMonth()+1 + '-' + secondDate.getDate()  )
                 var msg = "";
                 if ((newplandailycost-plandailycost) * diffDays > 0) {
-                    msg = " This will cos $" + Math.round((newplandailycost-plandailycost) * diffDays)/100 + "." 
+                    msg = " This will cos $" + Math.round((newplandailycost-plandailycost) * diffDays)/100 + ".";
+                    msg = msg.replace(/(.+\.\d)\.$/g, "$1") + "0.";
                 }
                 Acme.SigninView.render("userPlanChange", "Are you sure you want to change plan?" + msg)
                     .done(function() {
