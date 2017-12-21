7dc5a8972e589c76f23a36c3f26800600f76d171
Puck Willis <PWillis@pagemasters.com.au>
2017-12-21 14:07:51 +1100

    no message

--- static/development/js/infiniteScroll.js
+++ static/development/js/infiniteScroll.js
@@ -16,7 +16,7 @@ Acme.infiniteScroll = function(limit, count, feedModel) {
                 handler: function (direction) {
                     if (direction == 'down') {
                         self.feedModel.fetch($(this.element), waypoint);
-                        Waypoint.refreshAll();
+                        element: $('.loadMoreArticles');
                     }
                 }
             });
