dcb00be14bd5cdca8e4f671f82d384f0db19b729
Puck Willis <PWillis@pagemasters.com.au>
2018-03-05 09:39:41 +1100

    vid player v1

--- static/development/js/sdk/video-player.js
+++ static/development/js/sdk/video-player.js
@@ -84,11 +84,14 @@
                             mozallowfullscreen\
                             style="width: 100%; height: 100%; position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px;"></iframe>\
                         </div></div>';
+                    $(".featured")[0].innerHTML = content;    
                 }
             }
             
             if (typeof url !== 'undefined' && url !== "") {
                 var _player, _isPlaying = false;
+                console.log(this);
+
                 $.fancybox({
                     type: opts.type,
                     scrolling: opts.scrolling,
