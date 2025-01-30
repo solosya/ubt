51bb198e5533f987b8ac5c4ef3562e1d2ffa07db
inspheremayank <insphere.mayank@gmail.com>
2025-01-30 11:19:38 +0530

    name changes

--- static/development/js/weather.js
+++ static/development/js/weather.js
@@ -29,7 +29,7 @@
                     'America/San%20Antonio',
                     'America/San%20Francisco',
                     'America/Seattle',
-                    'America/Los%20Angels',
+                    'America/Los%20Angeles',
                     //'America/Toronto',
                     //'America/Vancouver',
 					'America/Washington%20DC',
